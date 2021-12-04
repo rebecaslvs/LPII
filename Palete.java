d06afe6bf75f8b484bc26f8156bf58e006cc1736
rebecaslvs <94875021+rebecaslvs@users.noreply.github.com>
2021-12-03 21:02:09 -0300

    Update Palete.java

--- Palete.java
+++ Palete.java
@@ -10,12 +10,12 @@ package superatacadista;
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class Palete 
-{
-    ArrayDeque<String> paletes_nivel1 = new ArrayDeque <> (3);
-    ArrayDeque<String> paletes_nivel2 = new ArrayDeque <> (3);
-    ArrayDeque<String> paletes_nivel3 = new ArrayDeque <> (3);
-    
+public class Palete {
+
+    ArrayDeque<String> paletes_nivel1 = new ArrayDeque<>(3);
+    ArrayDeque<String> paletes_nivel2 = new ArrayDeque<>(3);
+    ArrayDeque<String> paletes_nivel3 = new ArrayDeque<>(3);
+
     private Float n; //número de identificação do palete
     private String posicao; //A, B, C, D, E e F
     private int nivel; // nivel de empilhamento
@@ -76,95 +76,89 @@ public class Palete
     public void setIdentificador(String identificador) {
         this.identificador = identificador;
     }
-    
-    void empilhar()
+    public String toString()
     {
-    
-    ArrayDeque<String> paletes_nivel1 = new ArrayDeque <> (3);
-    ArrayDeque<String> paletes_nivel2 = new ArrayDeque <> (3);
-    ArrayDeque<String> paletes_nivel3 = new ArrayDeque <> (3);
-       
-    Scanner leitor = new Scanner(System.in);
-   
-    float peso_mercadoria =0; //característica do palete
+        return "{ID: "+identificador+"}";
+    }
+    void empilhar() {
+
+      //  ArrayDeque<String> paletes_nivel1 = new ArrayDeque<>(3);
+        //ArrayDeque<String> paletes_nivel2 = new ArrayDeque<>(3);
+        //ArrayDeque<String> paletes_nivel3 = new ArrayDeque<>(3);
+
+        Scanner leitor = new Scanner(System.in);
+
+        float peso_mercadoria = 0; //característica do palete
         String tipo_mercadoria = ""; //característica do palete
-        
+
         int escolha = 1; //para controle do menu de posições
-        int menu =0; //para controle do menu de ações
-        
-        int [] numero_a = new int [10000000];
-        int [] numero_b = new int [10000000];
-        
-        int posicao = 1;
-        
-       while(escolha==1)
-       {
-           escolha=0;
-           
-       System.out.println("Esse palete será inserido em qual posição(A, B, C, D, E ou F): ");
-       String menu_pp=leitor.next();
-        
-        if("A".equals(menu_pp)==true)
-            { 
-                if(paletes_nivel1.size()== 3)
-                {
+        int menu = 0; //para controle do menu de ações
+
+        int[] numero_a = new int[10000000];
+        int[] numero_b = new int[10000000];
+
+        int posicaoa = 1;
+        int posicaob = 1;
+
+        while (escolha == 1) {
+
+            System.out.println("Esse palete será inserido em qual posição(A, B, C, D, E ou F): ");
+            String menu_pp = leitor.nextLine();
+
+            if ("A".equals(menu_pp) == true) {
+                if (paletes_nivel1.size() == 3) {
                     System.out.println("POSIÇÃO DE EMPILHAMENTO CHEIA!");
-                } else
-                {
+                } else {
 
-                System.out.println("Qual o tipo de mercadoria? ");
-                tipo_mercadoria=leitor.next();
-                System.out.println("Qual o peso da mercadoria? ");
-                peso_mercadoria=leitor.nextFloat();
-                
-                String identificador=(menu_pp)+(numero_a[posicao]+1);
-                numero_a[posicao]++;
-                    
-                System.out.println("Palete adicionado a posição A e ao nível " +numero_a[posicao]);
-                paletes_nivel1.add(identificador);
+                    System.out.println("Qual o tipo de mercadoria? ");
+                    tipo_mercadoria = leitor.nextLine();
+                    System.out.println("Qual o peso da mercadoria? ");
+                    peso_mercadoria = leitor.nextFloat();
 
-                System.out.println("Você deseja empilhar um novo palete? Responda com 1 para sim ou digite qualquer outra tecla para não: ");
-                escolha=leitor.nextInt();
-                if(escolha!=1)
-                {
-                    menuprincipal();
+                    identificador = (menu_pp) + (numero_a[posicaoa] + 1);
+                    numero_a[posicaoa]++;
+
+                    System.out.println("Palete adicionado a posição A e ao nível " + numero_a[posicaoa]);
+                    paletes_nivel1.add(identificador);
+                    //paletes_nivel1.add("TESTE");
+                    //System.out.println(identificador);
+
+                    System.out.println("Você deseja empilhar um novo palete? Responda com 1 para sim ou digite qualquer outra tecla para não: ");
+                    escolha = leitor.nextInt();
+                    if (escolha != 1) {
+                        menuprincipal();
+                    }
+                }
+            } else if ("B".equals(menu_pp) == true) {
+                if (paletes_nivel1.size() == 3) {
+                    System.out.println("POSIÇÃO DE EMPILHAMENTO CHEIA!");
+                } else {
+                    System.out.println("Qual o tipo de mercadoria? ");
+                    tipo_mercadoria = leitor.nextLine();
+                    System.out.println("Qual o peso da mercadoria? ");
+                    peso_mercadoria = leitor.nextFloat();
+
+                    identificador = (menu_pp) + (numero_b[posicaob] + 1);
+                    numero_b[posicaoa]++;
+                    paletes_nivel2.add(identificador);
+                    System.out.println(identificador);
+
+                    System.out.println("Palete adicionado a posição B e ao nível " + numero_b[posicaob]);
+
+                    System.out.println("Você deseja empilhar um novo palete? Responda com 1 para sim ou digite qualquer outra tecla para não: ");
+                    escolha = leitor.nextInt();
+                    if (escolha != 1) {
+                        menuprincipal();
+                    }
                 }
             }
-            }else
-                if ("B".equals(menu_pp)==true)
-                        {
-                            if(paletes_nivel1.size()== 3)
-                            {
-                                 System.out.println("POSIÇÃO DE EMPILHAMENTO CHEIA!");
-                            }else
-                            {
-                            System.out.println("Qual o tipo de mercadoria? ");
-                            tipo_mercadoria=leitor.next();
-                            System.out.println("Qual o peso da mercadoria? ");
-                            peso_mercadoria=leitor.nextFloat();
-                            
-                            String identificador=(menu_pp)+(numero_b[posicao]+1);
-                            numero_b[posicao]++;
-                            paletes_nivel2.add(identificador);
-                           
-                            System.out.println("Palete adicionado a posição B e ao nível " +numero_b[posicao]);
-                            paletes_nivel2.add(identificador);
-                            
-                            System.out.println("Você deseja empilhar um novo palete? Responda com 1 para sim ou digite qualquer outra tecla para não: ");
-                            escolha=leitor.nextInt();
-                            if(escolha!=1)
-                            {
-                                menuprincipal();
-                            }
-                        }
-                        }
-       }
+        }
     }
-    void menuprincipal()
-    {
-        int menu=0;
+
+    void menuprincipal() {
+        int menu = 0;
         Palete p1 = new Palete("", 0f, "");
-        
+
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("****MENU DE OPÇÕES****");
@@ -177,19 +171,19 @@ public class Palete
         System.out.println("6-POSIÇÕES DE EMPILHAMENTO LIVRES");
         System.out.println("");
         System.out.println("DIGITE O NÚMERO DA OPÇÃO DESEJADA: ");
-        menu=leitor.nextInt();
-        
-        if(menu==1)
-        {
+        menu = leitor.nextInt();
+
+        if (menu == 1) {
             p1.empilhar();
-        }else
-            if(menu==3) //não está funcionando ainda
-            {
-                String busca;
-                System.out.println("Digite ID do palete que deseja consultar (formato ch.n): ");
-                busca=leitor.next();
-                
-                System.out.println(paletes_nivel1.getLast());
-            }
+        } else if (menu == 3) //não está funcionando ainda
+        {
+            String busca;
+            System.out.println("Digite ID do palete que deseja consultar (formato ch.n): ");
+            busca = leitor.next();
+        } else {
+            //System.out.println(paletes_nivel1);
+            System.out.println(paletes_nivel1.toString());
+        }
+
     }
 }
