e7bf4b9e37f6dd955e509327695530605d324eb2
rebecaslvs <94875021+rebecaslvs@users.noreply.github.com>
2021-12-05 10:57:45 -0300

    Update Palete.java

--- Palete.java
+++ Palete.java
@@ -19,9 +19,35 @@ public class Palete {
     private double peso; //peso da carga em quilogramas
     private String identificador; //junção da posição mais n (ch.n)
     
-        ArrayDeque<Palete> paletes_nivel1 = new ArrayDeque<>(3);
-        ArrayDeque<Palete> paletes_nivel2 = new ArrayDeque<>(3);
-        ArrayDeque<String> paletes_nivel3 = new ArrayDeque<>(3);
+    Scanner leitor = new Scanner(System.in);
+
+    float peso_mercadoria = 0; //característica do palete
+    String tipo_mercadoria = ""; //característica do palete
+
+    int escolha = 1; //para controle do menu de posições
+    int menu = 0; //para controle do menu de ações
+
+    int[] numero_a = new int[10000000];
+    int[] numero_b = new int[10000000];
+    
+    int cont_bebida=0;
+    int cont_cereal=0;
+    int cont_enlatado=0;
+    int cont_higiene=0;
+    int cont_limpeza=0;
+    
+    float peso_bebida=0;
+    float peso_cereal=0;
+    float peso_enlatado=0;
+    float peso_higiene=0;
+    float peso_limpeza=0;
+
+    int posicaoa = 1;
+    int posicaob = 1;
+    
+    ArrayDeque<Palete> paletes_nivel1 = new ArrayDeque<>(3);
+    ArrayDeque<Palete> paletes_nivel2 = new ArrayDeque<>(3);
+    ArrayDeque<String> paletes_nivel3 = new ArrayDeque<>(3);
 
     public Palete(String tipo, double peso, String identificador) { //adicionar o identificador aqui
         this.tipo = tipo;
@@ -81,6 +107,52 @@ public class Palete {
         return "{ID: "+identificador+"}";
     }
     
+    void menu_tipo()
+    {
+        System.out.println("*****TIPO DE MERCADORIA****");
+        System.out.println("1- BEBIDAS");
+        System.out.println("2- CEREAIS");
+        System.out.println("3- ENLATADOS");
+        System.out.println("4- HIGIENE PESSOAL");
+        System.out.println("5- MATERIAL DE LIMPEZA");
+        System.out.println("");
+        System.out.println("Digite o número correspondente ao tipo de mercadoria: ");
+        tipo_mercadoria = leitor.nextLine();
+        System.out.println("Qual o peso da mercadoria? ");
+        peso_mercadoria = leitor.nextFloat();
+        
+        if("1".equals(tipo_mercadoria) == true)
+        {
+            cont_bebida++;
+            tipo_mercadoria="BEBIDA";
+            peso_bebida= peso_bebida+peso_mercadoria;
+        }else
+            if("2".equals(tipo_mercadoria) == true)
+            {
+                cont_cereal++;
+                tipo_mercadoria="CEREAL";
+                peso_cereal= +peso_mercadoria;
+            }else
+                if("3".equals(tipo_mercadoria) == true)
+                {
+                    cont_enlatado++;
+                    tipo_mercadoria="ENLATADO";
+                    peso_enlatado= +peso_mercadoria;
+                }else
+                    if("4".equals(tipo_mercadoria) == true)
+                    {
+                        cont_higiene++;
+                        tipo_mercadoria="HIGIENE PESSOAL";
+                        peso_higiene= +peso_mercadoria;
+                    }else
+                        if("5".equals(tipo_mercadoria) == true)
+                        {
+                            cont_limpeza++;
+                            tipo_mercadoria="MATERIAL DE LIMPEZA";
+                            peso_limpeza= +peso_mercadoria;
+                        }
+    }
+    
     void menuprincipal()
     {
         Palete p1 = new Palete("", 0f, "");
@@ -104,7 +176,31 @@ public class Palete {
         {
             p1.empilhar();
             
-        } else if (menu == 3) //não está funcionando ainda
+        } else
+            if(menu==4)
+            {
+                System.out.println("****QUANTIDADE DE PRODUTOS POR TIPO DE MERCADORIA****");
+                System.out.println("BEBIDAS = "+ cont_bebida);
+                System.out.println("CEREAIS = "+ cont_cereal);
+                System.out.println("ENLATADOS = "+ cont_enlatado);
+                System.out.println("HIGIENE PESSOAL = "+ cont_higiene);
+                System.out.println("MATERIAL DE LIMPEZA = "+ cont_limpeza);
+                System.out.println("");
+                menuprincipal();
+            }else
+                if(menu==5)
+                {
+                System.out.println("****PESO TOTAL POR TIPO DE MERCADORIA****");
+                System.out.println("BEBIDAS = "+ peso_bebida + " quilos");
+                System.out.println("CEREAIS = "+ peso_cereal+ " quilos");
+                System.out.println("ENLATADOS = "+ peso_enlatado+ " quilos");
+                System.out.println("HIGIENE PESSOAL = "+ peso_higiene+ " quilos");
+                System.out.println("MATERIAL DE LIMPEZA = "+ peso_limpeza+ " quilos");
+                System.out.println("");
+                menuprincipal();
+                }
+            
+            if (menu == 3) //não está funcionando ainda
         {
             String busca;
             System.out.println("Digite ID do palete que deseja consultar (formato ch.n): ");
@@ -116,21 +212,7 @@ public class Palete {
     }
     
     void empilhar() {
-
-        Scanner leitor = new Scanner(System.in);
-
-        float peso_mercadoria = 0; //característica do palete
-        String tipo_mercadoria = ""; //característica do palete
-
-        int escolha = 1; //para controle do menu de posições
-        int menu = 0; //para controle do menu de ações
-
-        int[] numero_a = new int[10000000];
-        int[] numero_b = new int[10000000];
-
-        int posicaoa = 1;
-        int posicaob = 1;
-
+        
         while (escolha == 1) {
 
             System.out.println("Esse palete será inserido em qual posição(A, B, C, D, E ou F): ");
@@ -142,10 +224,8 @@ public class Palete {
                     System.out.println("POSIÇÃO DE EMPILHAMENTO CHEIA!");
                     menuprincipal();
                 } else {
-                    System.out.println("Qual o tipo de mercadoria? ");
-                    tipo_mercadoria = leitor.nextLine();
-                    System.out.println("Qual o peso da mercadoria? ");
-                    peso_mercadoria = leitor.nextFloat();
+                    menu_tipo();
+                    
 
                     identificador = (menu_pp) + (numero_a[posicaoa] + 1);
                     numero_a[posicaoa]++;
